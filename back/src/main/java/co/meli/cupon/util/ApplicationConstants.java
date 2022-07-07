43c484ec52434d8512070deacae10827ce09d9de
ahhernandez <ahhernandez@indracompany.com>
2022-07-06 19:01:20 -0500

    estructura del proyecto

---
+++ back/src/main/java/co/meli/cupon/util/ApplicationConstants.java
@@ -0,0 +1,6 @@
+package co.meli.cupon.util;
+
+public class ApplicationConstants {
+
+  public static final String API_VERSION = "/api/v1";
+}
---
+++ back/src/test/java/co/meli/cupon/CuponApplicationTests.java
@@ -0,0 +1,13 @@
+package co.meli.cupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
